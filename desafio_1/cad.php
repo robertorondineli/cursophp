11591282a72fb83a514c758d45f52f34e91c3d34
Roberto Rondineli Fernandes de Lima <betofl45v.8@gmail.com>
2024-08-22 18:15:28 -0300

    bla

---
+++ desafio_1/cad.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antecessor e Sucessor</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+      <h1>Antecessores e Sucessores do seu número</h1>
+    </header>
+    <a href="javascript:history.go(-1)">Voltar para a página anterior</a>
+</body>
+</html>
