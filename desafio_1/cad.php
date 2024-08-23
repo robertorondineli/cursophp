fc117e6cdcb4d41538e653c69f891cd85dabb121
Roberto Rondineli Fernandes de Lima <betofl45v.8@gmail.com>
2024-08-23 00:17:03 -0300

    a

--- desafio_1/cad.php
+++ desafio_1/cad.php
@@ -8,8 +8,16 @@
 </head>
 <body>
     <header>
-      <h1>Antecessores e Sucessores do seu número</h1>
+      <h1>Antecessor e Sucessor do seu número</h1>
     </header>
+    <?php 
+      $numero = $_GET['numero'];
+      echo "Antecessor: ". $numero - 1;     
+    ?>
+    <br>
+    <?php 
+      echo "Sucessor: ". $numero + 1;
+    ?>
     <a href="javascript:history.go(-1)">Voltar para a página anterior</a>
 </body>
 </html>
